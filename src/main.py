c26dfb351138e32412ff392a7f1da8b160d3b8f4
Léo Raclet <raclet.leo@proton.me>
2022-12-20 21:37:59 +0100

    requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+import torch
 
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+
+x_ones = torch.ones_like(x_data)  # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+
+x_rand = torch.rand_like(x_data, dtype=torch.float)  # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
